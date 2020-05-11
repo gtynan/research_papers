dc285af1110f38b56ba93e4d334af9d64c86fcde
gavin_tynan <gavin.tynan1@gmail.com>
2020-05-11 21:35:09 +0100

    fucntion name changed

--- high_dim_bt/src/high_dim_bt/nodes/data_science.py
+++ high_dim_bt/src/high_dim_bt/nodes/data_science.py
@@ -18,7 +18,7 @@ def get_outputs(model: Model1) -> Tuple[pd.Series, pd.Series]:
     '''
     Returns the fitted abilities and the model params in a series
     '''
-    fitted_abilities = model.get_fitted_ranking()
+    fitted_abilities = model.get_ranking()
     model_params = pd.Series(
         {"alpha": model.alpha, "tau_b": model.tau_b,
          "log_likelihood": model.ln_likelihood})
