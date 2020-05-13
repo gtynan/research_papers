bf579a7962bb7af7c00c4ffc104f337b20334e4d
gavin_tynan <gavin.tynan1@gmail.com>
2020-05-13 16:19:53 +0100

    additional docs and change of data

--- high_dim_bt/src/tests/conftest.py
+++ high_dim_bt/src/tests/conftest.py
@@ -14,11 +14,14 @@ def context():
 
 @pytest.fixture(scope='session')
 def dummy_data(context):
+    '''
+    Made up data to test against
+    '''
     data = {context.params['winner_col']: ["Tom", "Dick", "Tom", "Harry", "Tom"],
             context.params['loser_col']: ['Harry', 'Tom', 'Harry', 'Tom', 'Harry'],
-            context.params['date_col']: ['01/01/2020', '01/01/2020', '02/01/2020', '02/01/2020', '03/01/2020'],
-            context.params['winner_pts']: [100, 50, 200, 200, pd.NaT],
-            context.params['loser_pts']: [10, 20, 30, 10, 400]}
+            context.params['date_col']: ['01/01/2020', '01/01/2020', '02/01/2020', '02/01/2020', '02/01/2020'],
+            context.params['winner_pts']: [1, 5, 2, 1, pd.NaT],
+            context.params['loser_pts']: [.1, 2, 1, 3, 4]}
 
     return pd.DataFrame.from_dict(data)
 
@@ -27,6 +30,12 @@ def dummy_data(context):
 def c_data(context, dummy_data):
     '''
     Clean data is the same for all 3 models
+
+    Returns:
+        3 games:
+            W-Tom, L-Harry, Date-01
+            W-Tom, L-Harry, Date-02
+            W-Harry, L-Tom, Date-02
     '''
     c_data = clean_data(
         data=dummy_data,
